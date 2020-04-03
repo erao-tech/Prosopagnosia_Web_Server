ce4df9b49db104f7f3e822f819f0e3c7d9ed9b45
hongyixiao <hongyixiao@outlook.com>
2020-04-03 02:02:08 -0400

    api

--- ApiTester.py
+++ ApiTester.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3.7
 import requests
-url = 'http://127.0.0.1:5000/whichface'
-imageSource = open('/home/yixiao/Desktop/Steve-Mann-Jasleen-Arneja.jpg', 'rb')
-img = {'img': imageSource}
+url = 'http://54.161.20.123:5000/whichface'
+# url = 'http://localhost:5000/whichface'
+imageSource = open('C:\\Users\\felix\\Desktop\\Steve-Mann-Jasleen-Arneja-940x627.jpg', 'rb')
+img = {'img': imageSource,'img2':imageSource}
 response = requests.post(url, files=img)
 print(response.content)
